0f656f7b11e1411a84a865cff1788a3d0bc10359
justjoseph0 <iamonowu@gmail.com>
2025-01-12 01:04:24 -0800

    Update settings.py to configure email backend and add Paystack API keys

--- backend/settings.py
+++ backend/settings.py
@@ -12,21 +12,24 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g=m99lv+vyf_8pm@hl9ijw+)b&i4$zq6_5oo7sno0)6hu5&dj^'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 
-PAYSTACK_SECRET_KEY = "sk_test_903e42339453525d37e8756380380f5efac67c47"
-PAYSTACK_PUBLIC_KEY = "pk_test_086d6d7873611346ccc8c8980988aa9d608da45f"
+PAYSTACK_PUBLIC_KEY = os.getenv('PAYSTACK_PUBLIC_KEY')
+PAYSTACK_SECRET_KEY = os.getenv('PAYSTACK_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -205,11 +208,11 @@ REST_FRAMEWORK = {
 
 # to send mail
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'wurldblog@gmail.com' 
-EMAIL_HOST_PASSWORD = 'iishpcfhdpivovsw' 
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 
 
